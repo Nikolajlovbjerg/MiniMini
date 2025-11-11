7baf016daf6c9ad6cfa9dc4548dda1880ed2e6c0
Sommer4444 <eaa24ksom@students.eaaa.dk>
2025-11-11 09:49:13 +0100

    Added to User

--- BlazorApp/Service/UserRepo.cs
+++ BlazorApp/Service/UserRepo.cs
@@ -1,6 +1,26 @@
+using Core;
+
 namespace BlazorApp.Service;
 
 public class UserRepo
 {
-    
+    private List<User> mUsers;
+
+    public UserRepo()
+    {
+        mUsers = [new User { Name = "rip", Password = "1234", Role = "admin" },
+                  new User { Name = "rap", Password = "2345", Role="Normal"},
+                  new User { Name="rup", Password = "3456", Role="admin"}];
+    }
+
+    public User? ValidLogin(string name, string password)
+    {
+        foreach (User u in mUsers)
+            if (u.Name == name && u.Password == password)
+            {
+                return u;
+            }
+
+        return null;
+    }
 }
